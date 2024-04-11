ce6092d3300e86d5f861c699225412617c975bba
Rushikesh Chitte <rushichitte1998@gmail.com>
2024-04-11 09:08:53 +0530

    project setup

---
+++ src/config/index.js
@@ -0,0 +1,7 @@
+// TABLE HEADER NAMES
+export const EXISTIN_TYPE = "Existing type";
+export const APPLICATION_TYPE = "Application type";
+export const DOCUMENT_TO_BE_SUBMITTED = "Documents to be submitted";
+export const APPLICATION_DATE_AND_TIME = "Application date and time";
+export const APPROVAL = "Approval";
+export const REASON_FOR_REJECTION_OF_APPROVAL = "Reason for rejection of approval";
